308630eb679c742a3bd1c8666a411551d74f62c6
Eugene Ledovoy <anykey_skovorodker@rambler.ru>
2019-01-25 14:09:25 +0300

    renamed

--- Parsing/Container.cs
+++ Parsing/Container.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Parsing
 {
-    partial class ProcessData
+    internal class Container
     {
         public string BaseTag { get; } = "#noscript a";
         public string AtributeTag { get; } = "href";
@@ -21,13 +21,11 @@ namespace Parsing
         public string PrecipAtributeTag { get; } = "data-text";
 
 
-        private List<string> _nameList = new List<string>();
-        private List<string> _linkList = new List<string>();
+        internal List<string> _nameList = new List<string>();
+        internal List<string> _linkList = new List<string>();
 
-        private List<string> _tempTomorrowNight = new List<string>();
-        private List<string> _tempTomorrowDay = new List<string>();
-        private List<string> _precip = new List<string>();
-
-        private CancellationTokenSource _tokenSource;
+        internal List<string> _tempTomorrowNight = new List<string>();
+        internal List<string> _tempTomorrowDay = new List<string>();
+        internal List<string> _precip = new List<string>();
     }
 }
